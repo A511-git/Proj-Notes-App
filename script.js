98fd4db33324a9b74443d8448d6b55c4c0ff9585
A511-git <mhdatif0511@gmail.com>
2025-07-25 19:37:38 +0530

    deleted js cuz its buggy did a minor tweak in css to make modify buttons show above heading

---
+++ script.js
@@ -0,0 +1,11 @@
+const cardTemplate = document.querySelector("#card-template");
+
+const firstNote = document.querySelector(".first-note-container");
+
+const cardContainer = document.querySelector(".card-container");
+
+const addButton = document.querySelectorAll(".add-note");
+
+const addContentContainer = document.querySelector(".add-content-container");
+
+const fullPage = document.querySelector(".full-page");
